2479bb9e84152cf3643a64791043acfbd0d53f6e
Shanosha <wootenm1@gmail.com>
2026-03-31 20:03:08 -0400

    feature/created frontend api clients with axios

---
+++ frontend/src/clients/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export const token = () => localStorage.getItem('token')
+
+export const userClient = axios.create({
+    baseURL: BASE_URL+'/api/users',
+    headers: {
+        Authorization: `Bearer ${token()}`
+    }
+})
+
+export const projectClient = axios.create({
+    baseURL: BASE_URL+'/api/projects'
+})
+
+export const taskClient = axios.create({
+    baseURL: BASE_URL+'/api/tasks'
+})
+
+projectClient.interceptors.request.use((req) => {
+    if (token()) {
+        req.headers.Authorization = `Bearer ${token()}`
+    }
+    return req
+})
+
+taskClient.interceptors.request.use((req) => {
+    if (token()) {
+        req.headers.Authorization = `Bearer ${token()}`
+    }
+    return req
+})
